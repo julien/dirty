723ce755cc7d8dff5536741504748a351663bf77
Julien C <jcastelain@gmail.com>
2015-04-29 21:15:25 +0200

    initial commit

---
+++ dirty.go
@@ -0,0 +1,104 @@
+package main
+
+import (
+	"bufio"
+	"encoding/json"
+	"errors"
+	"io/ioutil"
+	"os"
+	"sync"
+)
+
+// Dirty is a JSON based storage
+type Dirty struct {
+	Path string
+
+	lock sync.RWMutex
+	docs map[string]interface{}
+}
+
+// NewDirty returns a new Dirty instance
+// and loads data given a file path
+func NewDirty(Path string) *Dirty {
+	d := &Dirty{
+		Path: Path,
+		docs: make(map[string]interface{}),
+	}
+	d.Read()
+	return d
+}
+
+// Set a key/value
+func (d *Dirty) Set(key string, val interface{}) {
+	if val == nil {
+		delete(d.docs, key)
+	} else {
+		if _, ok := d.docs[key]; ok == false {
+			d.lock.Lock()
+			d.docs[key] = val
+			d.lock.Unlock()
+		}
+	}
+	// d.flush()
+}
+
+func (d *Dirty) Flush() error {
+
+	f, err := os.Create(d.Path)
+	defer f.Close()
+	if err != nil {
+		return err
+	}
+
+	d.lock.Lock()
+	out, err := json.MarshalIndent(d.docs, "", " ")
+	d.lock.Unlock()
+
+	if err != nil {
+		return err
+	}
+
+	w := bufio.NewWriter(f)
+	_, err = w.WriteString(string(out))
+	w.Flush()
+	return nil
+}
+
+func (d *Dirty) Read() error {
+	if d.Path == "" {
+		return errors.New("No path")
+	}
+
+	data, err := ioutil.ReadFile(d.Path)
+	if err != nil {
+		return err
+	}
+
+	d.lock.Lock()
+	err = json.Unmarshal(data, &d.docs)
+	d.lock.Unlock()
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+// Get a key given it's name
+func (d *Dirty) Get(key string) interface{} {
+	return d.docs[key]
+}
+
+// Keys returns all keys
+func (d *Dirty) Keys() []string {
+	var keys []string
+	for k, _ := range d.docs {
+		keys = append(keys, k)
+	}
+	return keys
+}
+
+// All return all key/values
+func (d *Dirty) All() map[string]interface{} {
+	return d.docs
+}
---
+++ dirty_test.go
@@ -0,0 +1,75 @@
+package main
+
+import "testing"
+
+func TestNewDirty(t *testing.T) {
+	d := NewDirty("example.json")
+	if d.docs == nil {
+		t.Errorf("got %v", d.docs)
+	}
+}
+
+func TestSetVal(t *testing.T) {
+	d := NewDirty("example.json")
+	if d.docs == nil {
+		t.Errorf("got %v", d.docs)
+	}
+	d.Set("C", "three")
+	if d.Get("C") != "three" {
+		t.Errorf("got %v\n", d.Get("C"))
+	}
+	d.Flush()
+}
+
+func TestSetOtherVal(t *testing.T) {
+	d := NewDirty("example.json")
+	if d.docs == nil {
+		t.Errorf("got %v", d.docs)
+	}
+
+	d.Set("D", "four")
+	d.Set("E", "five")
+	if d.Get("D") != "four" {
+		t.Errorf("got %v\n", d.Get("C"))
+	}
+}
+
+func TestKeys(t *testing.T) {
+	d := NewDirty("example.json")
+	k := d.Keys()
+	if len(k) != 3 {
+		t.Errorf("got %v\n", len(k))
+	}
+}
+
+func TestAll(t *testing.T) {
+	d := NewDirty("example.json")
+	a := d.All()
+	if len(a) != 3 {
+		t.Errorf("got %v\n", len(a))
+	}
+}
+
+func TestSetNil(t *testing.T) {
+	d := NewDirty("example.json")
+	d.Set("C", nil)
+
+	a := d.All()
+	if len(a) != 2 {
+		t.Errorf("got %v\n", len(a))
+	}
+}
+
+func TestNoPath(t *testing.T) {
+	d := NewDirty("")
+	if d.Get("A") != nil {
+		t.Errorf("got %v\n", d.Get("A"))
+	}
+}
+
+func TestNonExistingPath(t *testing.T) {
+	d := NewDirty("non-existing.json")
+	if d.Get("A") != nil {
+		t.Errorf("got %v\n", d.Get("A"))
+	}
+}
